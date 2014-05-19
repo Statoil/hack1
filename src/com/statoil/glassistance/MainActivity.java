1e26b7901710132db7db78244bbeed12afa83816
flikka <kristian.flikka@gmail.com>
2014-05-19 14:23:22 +0200

    Non - functional camera code

--- src/com/statoil/glassistance/MainActivity.java
+++ src/com/statoil/glassistance/MainActivity.java
@@ -1,7 +1,15 @@
 package com.statoil.glassistance;
 
+import java.io.IOException;
+import java.util.logging.Logger;
+
 import android.app.Activity;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 public class MainActivity extends Activity {
 
@@ -9,7 +17,23 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-      
+        CameraInfo info = new CameraInfo();
+        Camera.getCameraInfo(0, info);
+        if (info.facing == CameraInfo.CAMERA_FACING_FRONT) {
+            Log.d("Nils", "Camera found");
+        }
+        
+        Camera camera = Camera.open();
+        
+       
+        try {
+			SurfaceView surfaceView = (SurfaceView)findViewById(R.id.capture_surface);
+			camera.setPreviewDisplay(surfaceView.getHolder());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        camera.startPreview();
     }
 
 }
