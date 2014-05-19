35637f74d8301186f6b78069adea224b3c8bb626
Christine Fløysand <chflo@statoil.com>
2014-05-19 19:58:06 +0200

    fixed picture upload

--- src/com/statoil/glassistance/MainActivity.java
+++ src/com/statoil/glassistance/MainActivity.java
@@ -1,11 +1,15 @@
 package com.statoil.glassistance;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
@@ -29,135 +33,122 @@ import android.widget.TextView;
 
 import com.google.android.glass.touchpad.Gesture;
 import com.google.android.glass.touchpad.GestureDetector;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
 public class MainActivity extends Activity {
-    private static final String TAG = "Glassistance";
-    private GestureDetector gestureDetector;
+	private static final String API_URL = "http://hackathon1.azurewebsites.net/api/image";
+	private static final String TAG = "Glassistance";
+	private GestureDetector gestureDetector;
 	private Camera camera;
 	private TextView text;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        setContentView(R.layout.hilfe);
-        
-        text = (TextView) findViewById(R.id.bottom_text);
+		setContentView(R.layout.hilfe);
 
-        setupGestures();
-    }
+		text = (TextView) findViewById(R.id.bottom_text);
 
-    private void setupGestures() {
-        gestureDetector = new GestureDetector(this);
-        gestureDetector.setBaseListener(new GestureDetector.BaseListener() {
-            @Override
-            public boolean onGesture(Gesture gesture) {
-                switch (gesture) {
-                    case TAP:
-                        startCamera();
-                        return true;
-                    case SWIPE_LEFT:
-                        takePicture();
-                        return true;
-                }
-                return false;
-            }
-        });
-    }
+		setupGestures();
+	}
 
-    protected void takePicture() {
+	private void setupGestures() {
+		gestureDetector = new GestureDetector(this);
+		gestureDetector.setBaseListener(new GestureDetector.BaseListener() {
+			@Override
+			public boolean onGesture(Gesture gesture) {
+				switch (gesture) {
+				case TAP:
+					startCamera();
+					return true;
+				case SWIPE_LEFT:
+					takePicture();
+					return true;
+				}
+				return false;
+			}
+		});
+	}
+
+	protected void takePicture() {
 		// TODO Auto-generated method stub
 		camera.takePicture(null, null, null, new Camera.PictureCallback() {
-			
+
 			@Override
 			public void onPictureTaken(byte[] data, Camera camera) {
 				releaseCamera();
 				text.setText("Jolly good!");
-				postQuestion(data);
-				
+				postQuestionAsJson(data);
+
 			}
 		});
 	}
 
 	@Override
-    public boolean onGenericMotionEvent(MotionEvent event) {
-        return gestureDetector.onMotionEvent(event);
-    }
+	public boolean onGenericMotionEvent(MotionEvent event) {
+		return gestureDetector.onMotionEvent(event);
+	}
 
-    void startCamera() {
-        camera = Camera.open();
-        text.setText("Swipe back to take a picture!");
-        try {
-            SurfaceView surfaceView = (SurfaceView) findViewById(R.id.surface_view);
-            camera.setPreviewDisplay(surfaceView.getHolder());
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        camera.startPreview();
-    }
-    
-       
-    @Override
-    protected void onStop() {
-    	releaseCamera();
-    	super.onStop();
-    	
-    }
-    
-    @Override
-    protected void onPause() {
-    	releaseCamera();
-    	super.onPause();
-    }
-    
-    void releaseCamera() {
-    	if(camera != null) {
-    		camera.stopPreview();
-    		camera.release();
-    		camera = null;
-    	}
-    }
-
-    void postQuestion(byte[] theQuestion) {
-		// Create a new HttpClient and Post Header
-		HttpClient httpclient = new DefaultHttpClient();
-		JSONObject json = null;
+	void startCamera() {
+		camera = Camera.open();
+		text.setText("Swipe back to take a picture!");
 		try {
-			json = new JSONObject("{ \"value:\"" + "\""
-					+ Base64.encode(theQuestion, Base64.DEFAULT) + "\"}");
-		} catch (JSONException e1) {
-			// TODO Auto-generated catch block
-			Log.e(this.getClass().getName(), e1.getMessage(), e1);
-			return; 
-		}
-
-		try {
-
-			HttpPost httppost = new HttpPost(
-					"http://hackathon1.azurewebsites.net/api/image");
-			StringEntity se;
-			se = new StringEntity(json.toString());
-
-			// Set HTTP parameters
-			httppost.setEntity(se);
-			httppost.setHeader("Content-Type", "application/json");
-
-			HttpResponse response = httpclient.execute(httppost);
-			String temp = EntityUtils.toString(response.getEntity());
-			Log.i("tag", temp);
-
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-
+			SurfaceView surfaceView = (SurfaceView) findViewById(R.id.surface_view);
+			camera.setPreviewDisplay(surfaceView.getHolder());
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+		camera.startPreview();
+	}
+
+	@Override
+	protected void onStop() {
+		releaseCamera();
+		super.onStop();
+
+	}
+
+	@Override
+	protected void onPause() {
+		releaseCamera();
+		super.onPause();
+	}
+
+	void releaseCamera() {
+		if (camera != null) {
+			camera.stopPreview();
+			camera.release();
+			camera = null;
 		}
 	}
 
-    byte[] getAnswer() {
+	void postQuestionAsJson(byte[] theQuestion) {
+		try {
+			AsyncHttpClient httpclient = new AsyncHttpClient();
+			HttpEntity entity = new StringEntity("\"" + Base64.encodeToString(theQuestion, Base64.DEFAULT) + "\"");
+			httpclient.post(getBaseContext(), API_URL, entity, "application/json", new PostResponseHandler());
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException("Oops", e);
+		}
+		
+	}
+	
+	void postQuestionAsFile(byte[] theQuestion) {
+		// Create a new HttpClient and Post Header
+		AsyncHttpClient httpclient = new AsyncHttpClient();
+		RequestParams params = new RequestParams();
+		params.put("question", new ByteArrayInputStream(theQuestion), "question.jpg");
+		httpclient.post(getBaseContext(), API_URL, null, params,
+				"application/json", new PostResponseHandler());
+	}
+
+	byte[] getAnswer() {
 		InputStream inputStream = null;
 		String resultAsString = "";
 		byte[] pic;
@@ -166,9 +157,9 @@ public class MainActivity extends Activity {
 			// create HttpClient
 			HttpClient httpclient = new DefaultHttpClient();
 
-			URI url = URI.create("http://hackathon1.azurewebsites.net/api/image");
+			URI url = URI.create(API_URL);
 			// make GET request to the given URL
-			HttpResponse httpResponse = httpclient.execute(new HttpGet(url ));
+			HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
 
 			// receive response as inputStream
 			inputStream = httpResponse.getEntity().getContent();
@@ -178,25 +169,38 @@ public class MainActivity extends Activity {
 				resultAsString = convertInputStreamToString(inputStream);
 				Log.e("Jepp", resultAsString);
 				pic = Base64.decode(resultAsString, Base64.DEFAULT);
-			    return pic;
-			}
-			else
+				return pic;
+			} else
 				resultAsString = "Did not work!";
-		    
+
 		} catch (Exception e) {
 			Log.d("InputStream", e.getLocalizedMessage());
 		}
 		return null;
 	}
-	 
-    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
-        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
-        String line = "";
-        String result = "";
-        while((line = bufferedReader.readLine()) != null)
-            result += line;
- 
-        inputStream.close();
-        return result;
-    }
+
+	private static String convertInputStreamToString(InputStream inputStream)
+			throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(
+				new InputStreamReader(inputStream));
+		String line = "";
+		String result = "";
+		while ((line = bufferedReader.readLine()) != null)
+			result += line;
+
+		inputStream.close();
+		return result;
+	}
+	
+	private final class PostResponseHandler extends AsyncHttpResponseHandler {
+		@Override
+		public void onSuccess(String content) {
+			text.setText("Great success!");
+		}
+		
+		@Override
+		public void onFailure(int statusCode, Throwable error, String content) {
+			text.setText("Epic fail");
+		}
+	}
 }
