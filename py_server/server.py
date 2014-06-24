979364532492555a3d2c8224d7b24c1d2876919e
Arve Skogvold <arvns@statoil.com>
2014-06-24 14:25:51 +0200

    jpg instead of png

--- py_server/server.py
+++ py_server/server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 
 def load_default_image():
-    image = open("static/default.png", "rb")
+    image = open("static/default.jpg", "rb")
     image_data = image.read()
     image.close()
     return image_data
@@ -19,13 +19,13 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/image.png", methods=["GET"])
+@app.route("/image.jpg", methods=["GET"])
 def get_image():
     response = make_response(app.image_data)
-    response.headers['Content-Type'] = "image/png"
+    response.headers['Content-Type'] = "image/jpeg"
     return response
 
-@app.route("/image.png", methods=["POST"])
+@app.route("/image.jpg", methods=["POST"])
 def write_image():
     app.image_data = base64.b64decode(request.data)
     return "Ok"
