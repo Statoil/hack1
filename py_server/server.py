7139ebb089e0534c1cfd1d114d9ccc07c6eec5a5
Arve Skogvold <arvns@statoil.com>
2014-06-24 14:16:49 +0200

    Created python server

---
+++ py_server/server.py
@@ -0,0 +1,35 @@
+from flask import Flask, make_response, render_template, request
+import base64
+app = Flask(__name__)
+
+
+
+def load_default_image():
+    image = open("static/default.png", "rb")
+    image_data = image.read()
+    image.close()
+    return image_data
+
+
+app.image_data = load_default_image()
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/image.png", methods=["GET"])
+def get_image():
+    response = make_response(app.image_data)
+    response.headers['Content-Type'] = "image/png"
+    return response
+
+@app.route("/image.png", methods=["POST"])
+def write_image():
+    app.image_data = base64.b64decode(request.data)
+    return "Ok"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
